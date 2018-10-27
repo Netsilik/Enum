c5459c477c95b4492adad27e7116b492cb9fb0fc
Jacco <rGd2n202YyBA>
2018-10-27 16:50:42 +0200

    Update class to 2018 coding standard

--- src/Enum.php
+++ src/Enum.php
@@ -2,12 +2,12 @@
 namespace Netsilik\Lib;
 
 /**
- * @package Scepino\Lib
- * @copyright (c) 2010-2016 Scepino (http://scepino.com)
- * @license EUPL-1.1 (European Union Public Licence, v1.1)
+ * @package       v\Lib
+ * @copyright (c) 2010-2018 Netsilik (http://netsilik.nl)
+ * @license       EUPL-1.1 (European Union Public Licence, v1.1)
  *
  * Example of usage:
- * 
+ *
  *   // Object creation
  *   final class Species extends Enum {
  *       protected $enum = array('human','monkey','alien');
@@ -19,27 +19,45 @@ namespace Netsilik\Lib;
  *   }
  *   checkSpecies(new Species('monkey'));
  */
- 
- 
+
 /**
  * Create list with set number of possible values
  */
-abstract class Enum {
-	protected $enum = array();
-	private $index;
+abstract class Enum
+{
+	/**
+	 * @var array $_enum The list of allowed enum values
+	 */
+	protected $_enum = [];
 	
-	final public function __construct($value) {
-		$index = array_search($value, $this->enum);
-		if ($index === false) {
-			trigger_error('Provided value is not part of this enumerated list', E_USER_WARNING);
+	/**
+	 * @var int $_index The index of the current enum value
+	 */
+	private $_index = -1;
+	
+	/**
+	 * Constructor
+	 *
+	 * @param mixed $value The enum value to set
+	 */
+	final public function __construct($value)
+	{
+		$index = array_search($value, $this->_enum);
+		if (false === $index) {
+			trigger_error('Provided value is not part of this enumerated list', E_USER_ERROR);
 		}
-		$this->index = $index;
+		$this->_index = $index;
 	}
 	
-	final public function __toString() {
-		if ($this->index === false) {
-			return '';
+	/**
+	 * @return mixed The value set through the constructor
+	 */
+	final public function __toString()
+	{
+		if ($this->_index === -1) {
+			return null;
 		}
-		return $this->enum[ $this->index ];
+		
+		return $this->_enum[ $this->_index ];
 	}
 }
